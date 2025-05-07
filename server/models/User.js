fb1f193dc8292630f3b0563dea32b9c9aeeecd51
Tamar-program <38327787347@mby.co.il>
2025-05-07 21:05:46 +0300

    Adding password encryption, login, token, and registration.

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require ("mongoose")
 const { stringify } = require("querystring")
 
+
 const userSchema = new mongoose.Schema({
     name: {
       type: String,
--- server/server.js
+++ server/server.js
@@ -13,19 +13,16 @@ app.use(express.json())
 app.use(express.static("public"))
 app.use(cors())
 
-<<<<<<< HEAD
-app.use("/user",require("./routers/userRouter"))
-app.use("/tourRouter",require("./routers/tourRouter"))
-app.use("/tourStationRouter",require("./routers/tourStationRouter"))
-=======
+app.use("/api/auth", require("./routers/authRouter"))
 app.use("/tour", require("./routers/tourRouter"))
 app.use("/tourStation", require("./routers/tourStationRouter"))
 app.use("/user", require("./routers/userRouter"))
->>>>>>> f8c50ae5c67550baff014511b60aaf05fbacc630
+
 
 app.get("/getser",(req,res) => {
 res.json("get")
 })
+
 mongoose.connection.once('open', () => {
     console.log(`connected to MongoDB`)
     app.listen(PORT, () => console.log(`The server running on port ${PORT}`))
