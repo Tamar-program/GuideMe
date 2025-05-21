1f877108cea747d0539ebd50fa733bb70ebea123
Yehudit-code <yehudit59501@gmail.com>
2025-05-21 20:46:58 +0300

    Fixed the errors from the merge

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,16 @@ import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/HomePage/Home';
 import AllTourStations from './components/TouristSites/AllTourStations';
-
 import { useDispatch,useSelector } from 'react-redux';
 import Login from './components/HomePage/Login';
+import MyMenuBar from './components/HomePage/MenuBar';
 
 function App() {
   const { token, role, user } = useSelector((state) => state.token);
 
   return (
     <div className="App">
-    { role=="Admin" ?<h1>ניווט למנהל </h1>:role=="User"?<h1>ניווט למשתמש </h1>:<></>}
+    { role=="Admin" ?<MyMenuBar/>:role=="User"?  <MyMenuBar/>:<MyMenuBar/>}
             <Routes>
                <Route path="/" element={<Home/>} />   
                <Route path="/TouristSites" element={<AllTourStations/>} />               
